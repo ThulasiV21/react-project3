dd416b218f93b101127369b8720d7dcaa7d3dce4
thulasigovardhan <thulasigovardhan@gmail.com>
2022-02-02 17:00:28 -0500

    Added reusable button component and Card UI css styling

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,5 +1,7 @@
 import {useState} from 'react';
+import Button from '../UI/Button';
 import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -14,14 +16,14 @@ const AddUser = (props) => {
     };
 
     return (
-    <Card>
+    <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <div>
             <label htmlFor='username'>Username</label>
             <input id="username" type='text' value={enteredName} onChange={nameChangeHandler}/>
             <label htmlFor='username'>Age (Years)</label>
             <input id="username" type='number' value={enteredName} onChange={nameChangeHandler}/>
-            <button type='submit'>Add User</button>
+            <Button type='submit'>Add User</Button>
             </div>
         </form>
 
--- src/Components/User/Age.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const Age = () => {
-    return <div>
-        
-    </div>
-};
-
-export default Age;
