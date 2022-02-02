d76ac2d3e58d997b7870dc1c6f019b8b6a56bc99
thulasigovardhan <thulasigovardhan@gmail.com>
2022-02-02 17:15:23 -0500

    Used useState hook to capture user input

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -4,15 +4,20 @@ import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredAge, enteredName);
     };
 
-    const [enteredName, setEnteredName] = useState('');
-
-    const nameChangeHandler = (event) => {
+    const usernameChangeHandler = (event) => {
         setEnteredName(event.target.value);
-        console.log(setEnteredName);
+    };
+
+    const uageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
@@ -20,9 +25,9 @@ const AddUser = (props) => {
         <form onSubmit={addUserHandler}>
             <div>
             <label htmlFor='username'>Username</label>
-            <input id="username" type='text' value={enteredName} onChange={nameChangeHandler}/>
+            <input id="username" type="text" onChange={usernameChangeHandler}/>
             <label htmlFor='username'>Age (Years)</label>
-            <input id="username" type='number' value={enteredName} onChange={nameChangeHandler}/>
+            <input id="username" type="number" onChange={uageChangeHandler}/>
             <Button type='submit'>Add User</Button>
             </div>
         </form>
